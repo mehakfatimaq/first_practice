1dd432261a169c4df025434e99d67487a8653508
Mehak Fatima <mehaksq@gmail.com>
2020-01-04 12:06:53 +0500

    rust library for factorial

---
+++ factorial/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn factorial_func (num:i32) {
+    if num == 0; {
+    println!("1!");
+    }
+    else {
+        for i in num (
+          i= num-1;
+          result = num*i;
+        )
+        println!("{}", result)
+    }
+}

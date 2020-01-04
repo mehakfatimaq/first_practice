780d0095bdd88a9fce82269e80fae2e2d67fa184
Mehak Fatima <mehaksq@gmail.com>
2020-01-04 12:23:01 +0500

    rust library crate for factorial

---
+++ factorial/target/package/factorial-0.1.0/target/debug/deps/factorial-c2b6e1311fe5054f.d
@@ -0,0 +1,7 @@
+C:\Users\HP\Desktop\first_practice\first_practice\factorial\target\package\factorial-0.1.0\target\debug\deps\factorial-c2b6e1311fe5054f.rmeta: src\lib.rs
+
+C:\Users\HP\Desktop\first_practice\first_practice\factorial\target\package\factorial-0.1.0\target\debug\deps\libfactorial-c2b6e1311fe5054f.rlib: src\lib.rs
+
+C:\Users\HP\Desktop\first_practice\first_practice\factorial\target\package\factorial-0.1.0\target\debug\deps\factorial-c2b6e1311fe5054f.d: src\lib.rs
+
+src\lib.rs:
